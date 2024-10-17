aabeef8bc2ace402441642eb3830df13995ed58d
jfga <jfga228@gmail.com>
2024-10-17 14:44:15 +0300

    Project Dependencies Setup

---
+++ app/src/main/java/com/loc/newsaggregatorapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.loc.newsaggregatorapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.loc.newsaggregatorapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            NewsAppTheme {
+
+            }
+        }
+    }
+}
